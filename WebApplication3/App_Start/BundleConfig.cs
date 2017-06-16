084b759968bdd23be54f43c3e606c12aee6b9734
tanuj <tnayanam@syr.edu>
2017-06-16 16:50:03 -0400

    Allowing minification for css and scripts

--- WebApplication3/App_Start/BundleConfig.cs
+++ WebApplication3/App_Start/BundleConfig.cs
@@ -25,7 +25,7 @@ namespace WebApplication3
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
-            BundleTable.EnableOptimizations = false;
+            BundleTable.EnableOptimizations = true;
         }
 
     }
