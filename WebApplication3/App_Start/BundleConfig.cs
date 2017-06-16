8c2fa4d60e16ec6b6c1fd020f9e27ba055909f10
tanuj <tnayanam@syr.edu>
2017-06-16 16:48:14 -0400

    Disbale minification of css and scripts file

--- WebApplication3/App_Start/BundleConfig.cs
+++ WebApplication3/App_Start/BundleConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Optimization;
+﻿using System.Web.Optimization;
 
 namespace WebApplication3
 {
@@ -26,6 +25,8 @@ namespace WebApplication3
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+            BundleTable.EnableOptimizations = false;
         }
+
     }
 }
