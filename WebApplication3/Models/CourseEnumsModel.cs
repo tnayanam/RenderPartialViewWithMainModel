d12f1c776713909b1dfeeb9977049016a08f6439
tanuj <tnayanam@syr.edu>
2017-06-29 17:12:14 -0400

    Adding display name for Model Property

--- WebApplication3/Models/CourseEnumsModel.cs
+++ WebApplication3/Models/CourseEnumsModel.cs
@@ -1,9 +1,14 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
 namespace WebApplication3.Models
 {
     public class CourseEnumsModel
     {
+        //[DisplayName("<YOUR NAME>")]
+        [Display(Name = "Select the Genre")]
         public CourseEnums OptionSelected { get; set; }
+
+        public string prop { get; set; }
 
         public string Error { get; set; }
     }
