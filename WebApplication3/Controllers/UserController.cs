a83c60bf7125ef16d4d3ff8c43c08a1aa58c40b7
tanuj <tnayanam@syr.edu>
2017-07-07 17:25:32 -0400

    Basic Input form

---
+++ WebApplication3/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserModel viewModel)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication3/Models/UserModel.cs
@@ -0,0 +1,10 @@
+﻿
+namespace WebApplication3.Models
+{
+    public class UserModel
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Age { get; set; }
+    }
+}
