4c24f2b71b06c632a0c972d21c2f35934efaaf32
tanuj <tnayanam@syr.edu>
2017-07-08 02:42:05 -0400

    Column type and length of Model Property

--- WebApplication3/Models/UserModel.cs
+++ WebApplication3/Models/UserModel.cs
@@ -1,11 +1,13 @@
 ﻿
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 namespace WebApplication3.Models
 {
     public class UserModel
     {
         [Required(ErrorMessage = "Name field cannot be blank")]
         [StringLength(10, MinimumLength = 5)]
+        [Column(TypeName = "varchar")]
         public string Name { get; set; }
         [Display(Name = "Confirm Name")]
         [Compare("Name", ErrorMessage = "Both field should have same value")]
