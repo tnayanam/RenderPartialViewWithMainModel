d11be29847d0d1a81c59a5cc324e41c4a7b0db8b
tanuj <tnayanam@syr.edu>
2017-07-08 02:58:05 -0400

    Automatic Migration Enabled

--- WebApplication3/Migrations/Configuration.cs
+++ WebApplication3/Migrations/Configuration.cs
@@ -1,15 +1,12 @@
 namespace WebApplication3.Migrations
 {
-    using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WebApplication3.Models.ApplicationDbContext>
     {
         public Configuration()
         {
-            AutomaticMigrationsEnabled = false;
+            AutomaticMigrationsEnabled = true;
         }
 
         protected override void Seed(WebApplication3.Models.ApplicationDbContext context)
