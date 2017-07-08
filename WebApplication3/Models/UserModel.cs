b56b7417b91f336c5702c2077d70b1398b5a81c7
tanuj <tnayanam@syr.edu>
2017-07-07 23:30:19 -0400

    Compare - model property compare to same value

--- WebApplication3/Models/UserModel.cs
+++ WebApplication3/Models/UserModel.cs
@@ -7,6 +7,9 @@ namespace WebApplication3.Models
         [Required(ErrorMessage = "Name field cannot be blank")]
         [StringLength(10, MinimumLength = 5)]
         public string Name { get; set; }
+        [Display(Name = "Confirm Name")]
+        [Compare("Name", ErrorMessage = "Both field should have same value")]
+        public string ConfirmName { get; set; }
         public int Id { get; set; }
         [Required]
         public int Age { get; set; }
