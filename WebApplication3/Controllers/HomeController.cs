5f9fa5064462dcbf858b917a3b5fb8fadd4afa1a
tanuj <tnayanam@syr.edu>
2017-07-09 09:00:00 -0400

    Anonymous Object, querystring, no hidden values, pass data to different backend controller

--- WebApplication3/Controllers/HomeController.cs
+++ WebApplication3/Controllers/HomeController.cs
@@ -1,11 +1,25 @@
-﻿using System.Web.Mvc;
+﻿using Microsoft.AspNet.Identity;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+
 
 namespace WebApplication3.Controllers
 {
     public class HomeController : Controller
     {
+
+        private ApplicationDbContext _context;
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
+            var userId = User.Identity.GetUserId();
+
+            //var email = _context.Users.Where(c => c.Id == userId).First().Email;
+            ViewBag.userId = userId;
             return View();
         }
 
---
+++ WebApplication3/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class OrderController : Controller
+    {
+        private ApplicationDbContext _context;
+        public OrderController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Order
+        public ActionResult Create()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Order viewModel)
+        {
+            return View();
+        }
+    }
+}
