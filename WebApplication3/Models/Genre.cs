ce4afc452dfb2de22823247fb0f3eababd211509
tnayanam <nayatanuj@gmail.com>
2017-07-16 14:51:31 -0400

    Initialize the properties NOT in constructor but in the definition itself

--- WebApplication3/Models/Genre.cs
+++ WebApplication3/Models/Genre.cs
@@ -5,5 +5,6 @@ namespace WebApplication3.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool isUnitNoEmptyInAllRow { get; set; } = true;
     }
 }
