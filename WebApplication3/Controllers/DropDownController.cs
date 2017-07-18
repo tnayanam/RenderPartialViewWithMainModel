34cd5ef7999ae2f44dd702f67a7dc23b3a792963
tanuj <tnayanam@syr.edu>
2017-07-18 15:18:57 -0400

    JQuery,Dropdown,Ajax Post Call,dynamic update page section with HTML content

--- WebApplication3/Controllers/DropDownController.cs
+++ WebApplication3/Controllers/DropDownController.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using WebApplication3.Models;
 
 namespace WebApplication3.Controllers
@@ -30,6 +31,28 @@ namespace WebApplication3.Controllers
             dropdown.SongList.Add(new SelectListItem { Text = "Six", Value = "6" });
             dropdown.SongList.Add(new SelectListItem { Text = "Seven", Value = "7" });
             return View(dropdown);
+        }
+
+        [HttpPost]
+        public JsonResult hello(int id)
+        {
+            List<SelectListItem> l1 = new List<SelectListItem>();
+            if (id == 1)
+            {
+                l1.Add(new SelectListItem { Text = "Dropdown value for One", Value = "1" });
+                l1.Add(new SelectListItem { Text = "1A", Value = "2" });
+                l1.Add(new SelectListItem { Text = "1B", Value = "3" });
+                l1.Add(new SelectListItem { Text = "1C", Value = "4" });
+            }
+            if (id == 2)
+            {
+                l1.Add(new SelectListItem { Text = "Dropdown value for Two", Value = "1" });
+                l1.Add(new SelectListItem { Text = "2A", Value = "2" });
+                l1.Add(new SelectListItem { Text = "2B", Value = "3" });
+                l1.Add(new SelectListItem { Text = "2C", Value = "4" });
+            }
+            var d = Json(l1);
+            return Json(l1);
         }
     }
 }
