598edf6db85bf31afd49e3537d481c5beb3576ac
tanuj <tnayanam@syr.edu>
2017-07-19 00:21:53 -0400

    Reflection -Check the Version Number of the ASP.Net MVC Application

--- WebApplication3/Controllers/DropDownController.cs
+++ WebApplication3/Controllers/DropDownController.cs
@@ -7,18 +7,19 @@ namespace WebApplication3.Controllers
     public class DropDownController : Controller
     {
         // GET: DropDown
-        public ActionResult Index()
+        public string Index()
         {
-            DropDownViewModel dropdown = new DropDownViewModel();
-            dropdown.SongList.Add(new SelectListItem { Text = "Four", Value = "4" });
-            dropdown.SongList.Add(new SelectListItem { Text = "Five", Value = "5" });
-            dropdown.SongList.Add(new SelectListItem { Text = "Six", Value = "6" });
-            dropdown.SongList.Add(new SelectListItem { Text = "Seven", Value = "7" });
-            dropdown.FirstName = "Tanuj";
-            dropdown.LastName = "Nayanam";
-            dropdown.RateInDollar = 23;
+            return typeof(Controller).Assembly.GetName().Version.ToString();
+            //DropDownViewModel dropdown = new DropDownViewModel();
+            //dropdown.SongList.Add(new SelectListItem { Text = "Four", Value = "4" });
+            //dropdown.SongList.Add(new SelectListItem { Text = "Five", Value = "5" });
+            //dropdown.SongList.Add(new SelectListItem { Text = "Six", Value = "6" });
+            //dropdown.SongList.Add(new SelectListItem { Text = "Seven", Value = "7" });
+            //dropdown.FirstName = "Tanuj";
+            //dropdown.LastName = "Nayanam";
+            //dropdown.RateInDollar = 23;
 
-            return View(dropdown);
+            //return View(dropdown);
         }
 
         // POST : DropDown
