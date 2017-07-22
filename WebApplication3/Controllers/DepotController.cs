6f06ba70c61817734518d2c1c68ac3d4cae533c8
tanuj <tnayanam@syr.edu>
2017-07-22 13:30:33 -0400

    DropdownList ViewBag Property

---
+++ WebApplication3/Controllers/DepotController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class DepotController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public DepotController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        // GET: Emp
+        public ActionResult Index()
+        {
+            //-------------------------------// source, // value to be submitted, name to display
+            ViewBag.Depots = new SelectList(_context.Depots, "Id", "Name");
+            return View();
+        }
+    }
+}
