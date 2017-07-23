1e0211ab9f66f41503a0bce38c1733d6cffb5dfb
tanuj <tnayanam@syr.edu>
2017-07-23 18:33:45 -0400

    Custom Html Helper for Image, Extension Method

---
+++ WebApplication3/CustomHtmlHelpers/CustomHtmlHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.CustomHtmlHelpers
+{
+    public static class CustomHtmlHelpers
+    {
+        public static IHtmlString Image(this HtmlHelper helper, string src, string alt)
+        {
+            TagBuilder tb = new TagBuilder("img");
+            tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
+            tb.Attributes.Add("alt", alt);
+            return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
