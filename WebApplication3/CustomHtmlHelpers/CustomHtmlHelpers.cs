771943a047d9e8f3b734aa63c7d038a6c959b925
tanuj <tnayanam@syr.edu>
2017-07-23 20:44:25 -0400

    Html Raw for avoiding the encoding

--- WebApplication3/CustomHtmlHelpers/CustomHtmlHelpers.cs
+++ WebApplication3/CustomHtmlHelpers/CustomHtmlHelpers.cs
@@ -8,23 +8,23 @@ namespace WebApplication3.CustomHtmlHelpers
         //public static IHtmlString Image(this HtmlHelper helper, string src, string alt)
 
         // BELOW FUNCTION REturns a basic string
-        //public static string Image(this HtmlHelper helper, string src, string alt)
-        //{
-        //    TagBuilder tb = new TagBuilder("img");
-        //    tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
-        //    tb.Attributes.Add("alt", alt);
-        //    //return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
-        //    return tb.ToString(TagRenderMode.SelfClosing);
-
-        //}
-
-        public static IHtmlString Image(this HtmlHelper helper, string src, string alt)
+        public static string Image(this HtmlHelper helper, string src, string alt)
         {
             TagBuilder tb = new TagBuilder("img");
             tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
             tb.Attributes.Add("alt", alt);
-            return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+            //return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+            return tb.ToString(TagRenderMode.SelfClosing);
 
         }
+
+        //public static IHtmlString Image(this HtmlHelper helper, string src, string alt)
+        //{
+        //    TagBuilder tb = new TagBuilder("img");
+        //    tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
+        //    tb.Attributes.Add("alt", alt);
+        //    return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+
+        //}
     }
 }
