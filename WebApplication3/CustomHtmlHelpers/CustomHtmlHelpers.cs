c4a59022a6a158adf3aba6f881d462d02e2bc896
tanuj <tnayanam@syr.edu>
2017-07-23 20:42:28 -0400

    HTML Encode

--- WebApplication3/CustomHtmlHelpers/CustomHtmlHelpers.cs
+++ WebApplication3/CustomHtmlHelpers/CustomHtmlHelpers.cs
@@ -5,12 +5,26 @@ namespace WebApplication3.CustomHtmlHelpers
 {
     public static class CustomHtmlHelpers
     {
+        //public static IHtmlString Image(this HtmlHelper helper, string src, string alt)
+
+        // BELOW FUNCTION REturns a basic string
+        //public static string Image(this HtmlHelper helper, string src, string alt)
+        //{
+        //    TagBuilder tb = new TagBuilder("img");
+        //    tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
+        //    tb.Attributes.Add("alt", alt);
+        //    //return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+        //    return tb.ToString(TagRenderMode.SelfClosing);
+
+        //}
+
         public static IHtmlString Image(this HtmlHelper helper, string src, string alt)
         {
             TagBuilder tb = new TagBuilder("img");
             tb.Attributes.Add("src", VirtualPathUtility.ToAbsolute(src));
             tb.Attributes.Add("alt", alt);
             return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+
         }
     }
 }
