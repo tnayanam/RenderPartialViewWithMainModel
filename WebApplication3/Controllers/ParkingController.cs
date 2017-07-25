5ec7197d796a4a7fed7d23ff86f257ca12afe324
tanuj <tnayanam@syr.edu>
2017-07-24 22:22:44 -0400

    Basic Get Request Form, RadioBox, Search Functionality

---
+++ WebApplication3/Controllers/ParkingController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class ParkingController : Controller
+    {
+        ApplicationDbContext _context;
+
+        public ParkingController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Parking
+        public ActionResult Index(string searchBy, string search)
+        {
+            if (searchBy == "Name")
+            {
+                return View(_context.Parkings.Where(p => p.Name.StartsWith(search)).ToList());
+
+            }
+            else if (searchBy == "Location")
+            {
+                return View(_context.Parkings.Where(p => p.Location.StartsWith(search)).ToList());
+
+            }
+            else
+            {
+                return View(_context.Parkings);
+            }
+
+            return View();
+        }
+    }
+}
--- WebApplication3/Models/Parking.cs
+++ WebApplication3/Models/Parking.cs
@@ -5,5 +5,6 @@ namespace WebApplication3.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Location { get; set; }
     }
 }
