78816b3ac09609437330c23545a909b6b2feb4cd
tanuj <tnayanam@syr.edu>
2017-08-01 13:42:06 -0400

    Error-exception handling thrown when resource is not found

---
+++ WebApplication3/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+// the previous git hub checkin workls for al the exception thrown at controller level. But what if resource itself is not found in that case we need to do all this stuff.
+
+namespace WebApplication3.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
