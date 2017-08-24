a01805ab6e5d89a87b256e8c6c90b1f3e141f616
tanuj <tnayanam@syr.edu>
2017-08-24 16:40:34 -0400

    FoolProof,validation

--- WebApplication3/ViewModel/CoverLetterViewModel.cs
+++ WebApplication3/ViewModel/CoverLetterViewModel.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Foolproof;
 using System.Web;
 using WebApplication3.CustomValidation;
 
@@ -7,11 +7,12 @@ namespace WebApplication3.ViewModel
     public class CoverLetterViewModel
     {
         [FileConstraint("pdf|doc|txt", ErrorMessage = "File type is not valid.")]
-        [Required]
         public HttpPostedFileBase File { get; set; }
 
-        [Required]
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+
+        [RequiredIf("FirstName", "tanuj", ErrorMessage = "Name field is required if first name is Tanuj")]
+        public string LastName { get; set; }
     }
 }
 
