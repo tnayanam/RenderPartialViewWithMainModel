0e67f775814e39dc7b07a67b8a1e3227daa3c0c1
tanuj <tnayanam@syr.edu>
2017-08-25 14:51:57 -0400

    Positive number check,validation

--- WebApplication3/ViewModel/CoverLetterViewModel.cs
+++ WebApplication3/ViewModel/CoverLetterViewModel.cs
@@ -17,7 +17,7 @@ namespace WebApplication3.ViewModel
 
         public string SelectedRoleType { get; set; }
 
-        [Required]
+        [Required] //positive number check
         [Range(1, int.MaxValue, ErrorMessage = "Only positive number allowed")]
         public int PositiveNumber { get; set; }
     }
