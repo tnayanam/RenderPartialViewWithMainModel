7a1fbea6400f42182756c25c4ad68a15c089a557
tanuj <tnayanam@syr.edu>
2017-10-18 11:14:58 -0400

    Console debug Write,selectmany,LINQ,multiple columns

--- WebApplication3/Controllers/CityController.cs
+++ WebApplication3/Controllers/CityController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web.Mvc;
 using WebApplication3.Models;
+using WebApplication3.ViewModel;
 
 
 namespace WebApplication3.Controllers
@@ -27,7 +29,27 @@ namespace WebApplication3.Controllers
 
             var f = _context.Cities.Select(c => new { Name = c.CityName + " " + c.CityName, Cityid = c.Id });
 
+            List<Project> Projects = new List<Project>();
+            Project proj1 = new Project { Name = "Cognizant", Cities = new List<string> { "Chennai", "Bangalore" } };
+            Project proj2 = new Project { Name = "TCS", Cities = new List<string> { "Chennai", "Bangalore", "Delhi" } };
 
+            Projects.Add(proj1);
+            Projects.Add(proj2);
+            // creates a collection of string found in the collection of native class.
+            // example: output: "Chennai", "Benaglore", "Chennai", "Bangalore", "Delhi"
+            // here Cities: needs to be collection
+            var f1 = Projects.SelectMany(p => p.Cities);
+            // example: output: "Chennai", "Benaglore", "Delhi"
+            var f2 = Projects.SelectMany(p => p.Cities).Distinct();
+
+            // Now suppse we want to display the project name too along with the city name
+            // Output: Cognizant-Chennai, Cognizant-Bangalore
+            var result = Projects.SelectMany(p => p.Cities, (Name, City) => new { ProjName = Name, CityName = City });
+            // Write to console.
+            foreach (var res in result)
+            {
+                Debug.Write(res.ProjName.Name + "-" + res.CityName);
+            }
             return View(r);
         }
 
---
+++ WebApplication3/ViewModel/Project.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace WebApplication3.ViewModel
+{
+    public class Project
+    {
+        public string Name { get; set; }
+        public List<string> Cities { get; set; }
+    }
+}
