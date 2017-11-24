d0531f4c5b86981a092df703074945e26eb6105b
tnayanam <nayatanuj@gmail.com>
2017-11-24 13:20:09 -0500

    Stringbuilder instead of string

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Text;
 
 namespace ConsoleApplication1
 {
@@ -66,142 +66,168 @@ namespace ConsoleApplication1
             //             * Also, the re3fderence types are stored in nHEAPS.            
             //             */
 
-            System.Console.WriteLine("Enter a number");
-            var input1 = Console.ReadLine();
+            //System.Console.WriteLine("Enter a number");
+            //var input1 = Console.ReadLine();
 
-            System.Console.WriteLine("Enter a number");
-            var input2 = Console.ReadLine();
-            var num1 = Convert.ToInt32(input1);
-            var num2 = Convert.ToInt32(input2);
+            //System.Console.WriteLine("Enter a number");
+            //var input2 = Console.ReadLine();
+            //var num1 = Convert.ToInt32(input1);
+            //var num2 = Convert.ToInt32(input2);
 
-            Console.WriteLine(num1 > num2 ? num1 : num2);
+            //Console.WriteLine(num1 > num2 ? num1 : num2);
 
-            var arr = new int[10] { 3, 2, 1, 6, 7, 5, 4, 3, 1, 0 };
-            Console.WriteLine("Length : {0}", arr.Length); // 10
+            //var arr = new int[10] { 3, 2, 1, 6, 7, 5, 4, 3, 1, 0 };
+            //Console.WriteLine("Length : {0}", arr.Length); // 10
 
-            var indexOf5 = Array.IndexOf(arr, 5); // 5
-            Console.WriteLine("Index of 5: {0}", indexOf5);
-            Array.Clear(arr, 0, 3);
-            Console.WriteLine();
-            foreach (var elem in arr)
-            {
-                Console.Write(elem + " ");
-            }
-            var arrDes = new int[10];
-            Console.WriteLine();
-            Array.Copy(arr, arrDes, 10);
-            foreach (var elem in arrDes)
-            {
-                Console.Write(elem + " ");
-            }
+            //var indexOf5 = Array.IndexOf(arr, 5); // 5
+            //Console.WriteLine("Index of 5: {0}", indexOf5);
+            //Array.Clear(arr, 0, 3);
+            //Console.WriteLine();
+            //foreach (var elem in arr)
+            //{
+            //    Console.Write(elem + " ");
+            //}
+            //var arrDes = new int[10];
+            //Console.WriteLine();
+            //Array.Copy(arr, arrDes, 10);
+            //foreach (var elem in arrDes)
+            //{
+            //    Console.Write(elem + " ");
+            //}
 
-            Array.Sort(arrDes);
-            Console.WriteLine();
-            foreach (var elem in arrDes)
-            {
-                Console.Write(elem + " ");
-            }
+            //Array.Sort(arrDes);
+            //Console.WriteLine();
+            //foreach (var elem in arrDes)
+            //{
+            //    Console.Write(elem + " ");
+            //}
 
-            Array.Reverse(arr);
+            //Array.Reverse(arr);
 
-            foreach (var elem in arrDes)
-            {
-                Console.Write(elem + " ");
-            }
-            Console.WriteLine();
-            var list = new List<int>() { 1, 5, 7, 9, 1, 2, 0 };
+            //foreach (var elem in arrDes)
+            //{
+            //    Console.Write(elem + " ");
+            //}
+            //Console.WriteLine();
+            //var list = new List<int>() { 1, 5, 7, 9, 1, 2, 0 };
 
-            list.Add(30);
-            list.AddRange(new int[3] { 45, 12, 14 });
+            //list.Add(30);
+            //list.AddRange(new int[3] { 45, 12, 14 });
 
-            foreach (var lst in list)
-            {
-                Console.Write(lst + " ");
-            }
-            Console.WriteLine();
-            list.Remove(1);
+            //foreach (var lst in list)
+            //{
+            //    Console.Write(lst + " ");
+            //}
+            //Console.WriteLine();
+            //list.Remove(1);
 
-            foreach (var lst in list)
-            {
-                Console.Write(lst + " ");
-            }
-            Console.WriteLine();
-            for (int i = 0; i < list.Count; i++)
-            {
-                if (list[i] == 1)
-                {
-                    list.Remove(list[i]);
-                }
-            }
-            foreach (var lst in list)
-            {
-                Console.Write(lst + " ");
-            }
-            Console.WriteLine();
+            //foreach (var lst in list)
+            //{
+            //    Console.Write(lst + " ");
+            //}
+            //Console.WriteLine();
+            //for (int i = 0; i < list.Count; i++)
+            //{
+            //    if (list[i] == 1)
+            //    {
+            //        list.Remove(list[i]);
+            //    }
+            //}
+            //foreach (var lst in list)
+            //{
+            //    Console.Write(lst + " ");
+            //}
+            //Console.WriteLine();
 
-            //list.RemoveAll()
+            ////list.RemoveAll()
 
-            var r = list.IndexOf(9);
-            var r1 = list.LastIndexOf(1);
-            var t = list.Contains(7);
-            list.Clear(); // removes all elemnt from list
-            //list.Count;
-
-
-            string e = "er";
-            e = "dsfd";
-
-            // String are Immutable, means their value cannot be modified. If you try to modify a new memory is allocated and 
-            //old memory still exists which will be deleted by garbage collection.
-            // ImmmutABLE MEANING VALUES CANNOT be changed, if we try to change it will return a new object wth new value.
-            string test = "Tanuj Nayanam ";
-            Console.WriteLine("Trim '{0}'", test.Trim()); // Removes leading and trailing spaces.
-            Console.WriteLine("ToUpper '{0}'", test.ToUpper()); // Removes leading and trailing spaces.
-
-            var newStringarr = test.Split(' '); // splits string based on the delimiter and returns the array
-            Console.WriteLine(newStringarr[0]);
-            Console.WriteLine(test.Substring(3));
-
-            Console.WriteLine(test.Replace("Tanuj", "Helo"));
-
-            var tr = "";
-            if (string.IsNullOrEmpty(tr))
-                Console.WriteLine("Empty Or Null");
-            else
-                Console.WriteLine("Empty Or Null");
-
-            var tr1 = " ";
-            if (string.IsNullOrWhiteSpace(tr1))
-                Console.WriteLine("Empty Or whiutesaoace");
-            else
-                Console.WriteLine("Not");
-
-            var tr3 = "dfd";
-            if (string.IsNullOrEmpty(tr3))
-                Console.WriteLine("Empty Or Null");
-            else
-                Console.WriteLine("Not");
-
-            string tr4 = null;
-
-            if (string.IsNullOrEmpty(tr4))
-                Console.WriteLine("Empty Or Null");
-            else
-                Console.WriteLine("Not");
+            //var r = list.IndexOf(9);
+            //var r1 = list.LastIndexOf(1);
+            //var t = list.Contains(7);
+            //list.Clear(); // removes all elemnt from list
+            ////list.Count;
 
 
-            var qw = "24";
-            var age = Convert.ToInt32(qw); // we cannot use normal casting here.
+            //string e = "er";
+            //e = "dsfd";
 
-            float price = 34.8f;
-            Console.WriteLine(price.ToString("C"));
-            Add(8);
+            //// String are Immutable, means their value cannot be modified. If you try to modify a new memory is allocated and 
+            ////old memory still exists which will be deleted by garbage collection.
+            //// ImmmutABLE MEANING VALUES CANNOT be changed, if we try to change it will return a new object wth new value.
+            //string test = "Tanuj Nayanam ";
+            //Console.WriteLine("Trim '{0}'", test.Trim()); // Removes leading and trailing spaces.
+            //Console.WriteLine("ToUpper '{0}'", test.ToUpper()); // Removes leading and trailing spaces.
+
+            //var newStringarr = test.Split(' '); // splits string based on the delimiter and returns the array
+            //Console.WriteLine(newStringarr[0]);
+            //Console.WriteLine(test.Substring(3));
+
+            //Console.WriteLine(test.Replace("Tanuj", "Helo"));
+
+            //var tr = "";
+            //if (string.IsNullOrEmpty(tr))
+            //    Console.WriteLine("Empty Or Null");
+            //else
+            //    Console.WriteLine("Empty Or Null");
+
+            //var tr1 = " ";
+            //if (string.IsNullOrWhiteSpace(tr1))
+            //    Console.WriteLine("Empty Or whiutesaoace");
+            //else
+            //    Console.WriteLine("Not");
+
+            //var tr3 = "dfd";
+            //if (string.IsNullOrEmpty(tr3))
+            //    Console.WriteLine("Empty Or Null");
+            //else
+            //    Console.WriteLine("Not");
+
+            //string tr4 = null;
+
+            //if (string.IsNullOrEmpty(tr4))
+            //    Console.WriteLine("Empty Or Null");
+            //else
+            //    Console.WriteLine("Not");
+
+
+            //var qw = "24";
+            //var age = Convert.ToInt32(qw); // we cannot use normal casting here.
+
+            //float price = 34.8f;
+            //Console.WriteLine(price.ToString("C"));
+            //Add(8);
+
+            var stringbuilder = new StringBuilder("oops");
+            stringbuilder.Append('-', 10);
+
+            Console.WriteLine(stringbuilder);
+            stringbuilder.AppendLine(); // adds a new line.
+
+            Console.WriteLine(stringbuilder);
+            stringbuilder.Append("Header");
+            stringbuilder.AppendLine();
+
+            Console.WriteLine(stringbuilder);
+            stringbuilder.Append('-', 10);
+
+            Console.WriteLine(stringbuilder);
+            stringbuilder.Replace('-', '+');
+            stringbuilder.Insert(0, "Hell");
+            stringbuilder.Remove(0, 4);
+
+            Console.WriteLine(stringbuilder);
+            Console.WriteLine(stringbuilder[0]);
 
         }
         public static void Add(int a, int r = 9)
         {
             Console.WriteLine("OK!");
         }
+
+        //    String Builder is a Mutable String unlike string class, so any changes you make is made to the same string rather
+        //a new one which is the case with String class. It is not meant for seaching though.It is more for manipulating.
+
+        // String does not have functionlke indexof lastindex of and al of that.
 
     }
 }
--- WebApplication3/App_Start/FilterConfig.cs
+++ WebApplication3/App_Start/FilterConfig.cs
@@ -31,6 +31,10 @@ namespace WebApplication3
  * 
  * 
  
+
+
+
+
      
      */
 
