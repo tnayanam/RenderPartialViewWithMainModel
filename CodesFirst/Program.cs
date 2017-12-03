2babf7bebfd77a94351537342d478aa4c93a1ae0
tnayanam <nayatanuj@gmail.com>
2017-12-03 12:22:16 -0500

    Model added as per the relationship in Code First

--- CodesFirst/Program.cs
+++ CodesFirst/Program.cs
@@ -1,5 +1,39 @@
-﻿namespace CodesFirst
+﻿using System.Collections.Generic;
+
+namespace CodesFirst
 {
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public CourseLevel Level { get; set; } // this is enum property, one course can have one level
+        public float FullPrice { get; set; }
+        public Author Author { get; set; } // navigational property, one course can have one author too
+        public IList<Tag> Tags { get; set; } // one course can have multiple tags
+    }
+
+    public class Author
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; } // one author can have many courses
+    }
+
+    public class Tag
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<Course> Courses { get; set; }  // Many to Many relationship b/w tag and course
+    }
+
+    public enum CourseLevel
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
+
     class Program
     {
         static void Main(string[] args)
