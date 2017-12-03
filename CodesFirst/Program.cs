106a0959aa54f3c426247bd28cd22719e59c180a
tnayanam <nayatanuj@gmail.com>
2017-12-03 12:10:49 -0500

    Anothe empty soln for code first study

---
+++ CodesFirst/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CodesFirst
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
+//install-package entityframework -Version 6.1.3 -projectName CodesFirst
