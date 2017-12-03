aca41d8f88dd72ca2c95e77ddcc8ae88ab714173
tnayanam <nayatanuj@gmail.com>
2017-12-03 01:14:10 -0500

    Add new table to DB and Update model and what is ORM

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DbFirst
+﻿namespace DbFirst
 {
     class Program
     {
@@ -13,3 +7,19 @@ namespace DbFirst
         }
     }
 }
+
+
+// ORM: Object Relationship Mapper maps between our code Models called as "Conceptual Model" and the Database Model called as 
+// Storage Models 
+// ORM maps between these two.
+
+/*
+ * What is an edmx file:
+ * An .edmx file is an XML file that defines a conceptual model , a storage model ,
+ *  and the mapping between these models. An .edmx file also contains information that is used by the 
+ * ADO.NET Entity Data Model Designer (Entity Designer) to render a model graphically
+ * Add new table: SO in database you added a new table
+ * IN order to get than Model ionto code. right click on an empty space in the edmx desinger file and click on Update from DB
+ * there select the checkbox of stuff you want to add and then click on finish
+
+ */
