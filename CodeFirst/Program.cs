9541ffff266765d0cf17efcf1053545869775f10
tnayanam <nayatanuj@gmail.com>
2017-12-02 22:54:05 -0500

    COde First Migration from Scratch, Connection String

---
+++ CodeFirst/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity;
+
+namespace CodeFirst
+{
+    public class Test
+    {
+        public int Id { get; set; }
+    }
+
+    public class AppDBContext : DbContext
+    {
+        public DbSet<Test> Tests { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
