62d7ae0117ef2b6411af4d7bed11ac7b280fc76b
tnayanam <nayatanuj@gmail.com>
2017-12-03 19:44:03 -0500

    Adding Reference data application specific

---
+++ PlutoContext/Migrations/201712040040323_seeddata.cs
@@ -0,0 +1,21 @@
+namespace PlutoContext.Migrations
+{
+    using System.Data.Entity.Migrations;
+
+    public partial class seeddata : DbMigration
+    {
+        public override void Up()
+        {
+            //Sql("Write Insert Statements Here.");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
+/*
+ * How to add some reference data to the database
+ * Run empty update-database
+ * and add all the SQL command you want for insedrtion.
+ * */
--- PlutoContext/Migrations/Configuration.cs
+++ PlutoContext/Migrations/Configuration.cs
@@ -26,3 +26,5 @@ namespace PlutoContext.Migrations
         }
     }
 }
+
+
