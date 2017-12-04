174c85a6705b55278be1d40d77e056f85288c31a
unknown <tnayanam@syr.edu>
2017-12-04 11:02:15 -0500

    Process explorer to find the dll version

--- WebApplication3/Extension/Helper.cs
+++ WebApplication3/Extension/Helper.cs
@@ -12,3 +12,7 @@ namespace WebApplication3.Extension
         }
     }
 }
+
+/*
+ * If you want to know a dll version then just open Process Explorer and you can check the dll that are being used. process explorer in microsoft provided product.
+ */
