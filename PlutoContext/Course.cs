459e58237ad7a39af84cf4782ce1d8d9715bb898
tnayanam <nayatanuj@gmail.com>
2017-12-03 22:20:19 -0500

    Table Name convention if not maitained in DB then map domain to DB table

--- PlutoContext/Course.cs
+++ PlutoContext/Course.cs
@@ -4,6 +4,11 @@ namespace PlutoContext
     using System.ComponentModel.DataAnnotations.Schema;
 
     [Table("Courses")]
+    // Ideally we should have table name as Plural form of our Domain Class
+    // So that entity framework can eaily map them
+    // If that is not the case in legacy code and all, then inorder to tell the framework that
+    // this domain class belongs to a particular table in DB we use below approach
+    //[Table("tbl_Courses")] 
     public partial class Course
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
