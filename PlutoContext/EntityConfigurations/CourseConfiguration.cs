68e0dbff14464c0d2a2f8cf7c132840867370af6
unknown <tnayanam@syr.edu>
2017-12-05 09:53:09 -0500

    IQueryable and IEnumerable Difference

--- PlutoContext/EntityConfigurations/CourseConfiguration.cs
+++ PlutoContext/EntityConfigurations/CourseConfiguration.cs
@@ -20,3 +20,25 @@ namespace PlutoContext.EntityConfigurations
         }
     }
 }
+
+/*
+ * LINQ to query objects
+ * LINQ to query Datatables
+ * LINQ to query DB
+ */
+
+/*
+ * Difference between IQueryable and IEnummerable
+ * IEnummerable is best to query data from in-memory collections like List,Array
+ * While running the Query IEnummerable loads the entire Select query from server side and all the filter will be applied on client side.
+ * IEnummerable does not support Lazy Loading
+ * Supports deferred execution.
+ * IQuereyable
+ * It supports Lazy Loading
+ * It is best to query data from Remote Services and Database as the queries along with its filters gets executed on server side
+ * Suppots lazy loading hence good for Paging Scenario.
+ * Supports deferred execution.
+ * 
+ * 
+ * 
+ */
