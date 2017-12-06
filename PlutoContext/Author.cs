1cee00580ff8e7b95539b8bf809e49855d3a6d4d
unknown <tnayanam@syr.edu>
2017-12-06 12:04:29 -0500

    Repository Pattern Definition

--- PlutoContext/Author.cs
+++ PlutoContext/Author.cs
@@ -22,3 +22,22 @@ namespace PlutoContext
         public virtual ICollection<Course> Courses { get; set; }
     }
 }
+
+/*
+ * Repository Pattern: It works/mediates between the domain and data mapping layers, acting like an IN-MEMORY COLLECTION of domain objects 
+ * Suppose we need to run same query again and again then we need to write that query in many controller actions, but if we use repository pattern then we can write the query at one place and
+ * call it from there. thus minimizes duplicate query.
+ * Entitiy Framework is a Persistance Framework
+ * Different type of O/RM -> Object Relation Mapper
+ * ADO.Net
+ * LINQ to SQL
+ * Entity Framework v1
+ * nHibernate
+ * Entity Framework v4
+ * Using repository pattern, it decouples our application from persistence framework. so tomorrow if we want to use another framework instead of entity framework we will have to do minimal changes
+ * Decouple the application from Entity Framework 
+ * 
+ * 
+ */
+
+
