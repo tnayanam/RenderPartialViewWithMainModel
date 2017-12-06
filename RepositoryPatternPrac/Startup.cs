f81317f26db46f5362af8ebc1d9704ae32f64719
unknown <tnayanam@syr.edu>
2017-12-06 12:36:33 -0500

    Empty solution added, preparing for the Repository pattern

---
+++ RepositoryPatternPrac/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RepositoryPatternPrac.Startup))]
+namespace RepositoryPatternPrac
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
