9f66afaa6ed4fcecf988f118a2a0d62c252e3677
tnayanam <nayatanuj@gmail.com>
2017-12-09 03:02:08 -0500

    Create a WCF Service

---
+++ HelloService/HelloService.cs
@@ -0,0 +1,18 @@
+﻿namespace HelloService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HelloService" in both code and config file together.
+    public class HelloService : IHelloService
+    {
+        public string GetMessage(string name)
+        {
+            return "Hello " + name;
+        }
+    }
+}
+
+/*//How did we create this service
+ * first created a class library (dll)
+ * then in the same project added one WCF Service
+ * 
+ */
+
---
+++ HelloService/IHelloService.cs
@@ -0,0 +1,12 @@
+﻿using System.ServiceModel;
+
+namespace HelloService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IHelloService" in both code and config file together.
+    [ServiceContract] /// this is making this a WCF Service
+    public interface IHelloService
+    {
+        [OperationContract] // this is making sure that this method is avialbale for the clients
+        string GetMessage(string name); // this is the service a client can invoke
+    }
+}
