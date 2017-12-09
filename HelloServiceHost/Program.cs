a931f0df69c3015155a200d24f8dbd710f9f1d94
tnayanam <nayatanuj@gmail.com>
2017-12-09 12:22:10 -0500

    Host started hosting the application

--- HelloServiceHost/Program.cs
+++ HelloServiceHost/Program.cs
@@ -1,15 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+using System.ServiceModel;
 namespace HelloServiceHost
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            using (ServiceHost host = new ServiceHost(typeof(HelloService.HelloService)))
+            {
+                host.Open();
+                System.Console.WriteLine("Host Started" + DateTime.Now);
+            }
         }
     }
 }
+
+/*
+ * Now we are creating a host which will host this WCF Service
+ * for that in the  separate console application we need to add the DLL of the WCF Service created in lst commit
+ * and also we need to add refernce of system.serviceModdel
+ * SO here our aim is to host the service via this console application, we want to create two end points. so let's change the
+ * config file.
+ * 
+ * 
+ * Its all little tough but you will get eventually used to it. For now I had to run the host in Administrator Mode.
+ */
+
+
