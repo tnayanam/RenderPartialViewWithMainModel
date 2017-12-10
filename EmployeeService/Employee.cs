707c4239ae6adc77f6cd790b48588d6da605b6b4
tnayanam <nayatanuj@gmail.com>
2017-12-10 17:37:39 -0500

    Data Member Data COntract on Complex type

--- EmployeeService/Employee.cs
+++ EmployeeService/Employee.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Runtime.Serialization;
 
 namespace EmployeeService
 {
+    [DataContract]
     public class Employee
     {
+        [DataMember]
         public int Id { get; set; }
+        [DataMember]
         public string Name { get; set; }
+        [DataMember]
         public string Gender { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; } //this will not get sreialized
     }
 }
+
+/*
+ * By default all the public properties are serialized but the private proerpties are not serialized, in Employee class all its property will be
+ * serialized as they are public
+ * However we can also use [Serializable] attribute to serialize but thne we don't have any control n what to serialize r not. everything gets
+ * serialized. alll properties privwte or public
+ * 
+ * If we out [DataContract] attirubte on the class ex: employee then we have full control on what we want to get serialized. All those property
+ * whish will have [DataMember] will only get serialized. 
+ * 
+ */
