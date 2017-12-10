8e34bef55972abe839b59656a3db1fc880927e49
tnayanam <nayatanuj@gmail.com>
2017-12-09 21:37:58 -0500

    Fresh start of a WCF Service - created a library with entity class

---
+++ EmployeeService/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeService
+{
+    public class Class1
+    {
+    }
+}
+
+
+/*
+ * Initial Set up done was in SSMS created a database named sample and then created a table names tblEMployee and created two sprocs
+ * Now creating this Class Library (dll) meaning service 
+ */
