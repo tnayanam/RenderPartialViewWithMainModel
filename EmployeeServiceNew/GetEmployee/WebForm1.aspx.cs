d7a7581cbc5f27c60fd01a344891b4c163e6a9ee
unknown <tnayanam@syr.edu>
2017-12-11 10:49:24 -0500

    Added Brand New WCF Basic Service Complete flow

---
+++ EmployeeServiceNew/GetEmployee/WebForm1.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GetEmployee
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var client = new EmployeeService.EmployeeServiceClient();
+            Label1.Text = client.GetEmployee();
+            Console.WriteLine(client.GetEmployee()); 
+        }
+    }
+}
