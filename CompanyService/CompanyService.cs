c7643a3b777bdbce4747467fe10c76b762f8d287
unknown <tnayanam@syr.edu>
2017-12-13 13:13:59 -0500

    Various ways of adding known types to the service in case of inheritance

--- CompanyService/CompanyService.cs
+++ CompanyService/CompanyService.cs
@@ -17,6 +17,33 @@
     }
 }
 
-
 // Kindly note that as we created wcf client we had to right click on its referenec and all a service reference and add the endpoint of the WSDL so that proxy class can
 // be created by client to consume the WCF Service.
+
+// How to allow the Clients to access property of inherited classes and not just base class.
+/*
+ * 1. [KnownTypes] on Base class : But this option is global, so all services and contracts are going to know about it, as long as their base class is same. ex: Employee
+ * 2. [ServiceKnowType] on the Interface of the service itself. if we do this then this derived classes are respected only in that ServiceContract only.
+ * Now if we have another service where we want this knowntypes then we again will have to add [ServieKnownType] there
+ * exam[le
+ *  [ServiceContract]
+ *  [ServiceKnownType(typeof(FullTimeEmployee))]
+ *  [ServiceKnownType(typeof(PartTimeEmployee))]
+    public interface IEmployeeService
+    {
+        [OperationContract]
+        Employee GetEmployee(int id);
+
+        [OperationContract]
+        void SaveEmployee(Employee employee);
+    }
+ * 
+ * 3. Now suppose we want more granualarity and we want to expose the derived classes to only one of the operation contract then we can apply the [ServiceKnownType] to a particular
+ * method
+ *  [OperationContract]
+ *  [ServiceKnownType(typeof(PartTimeEmployee))]
+ *  [ServiceKnownType(typeof(FullTimeEmployee))]
+ *  Employee GetEmployee(int id);
+ *  4. We can add the knowntypes in config file also: details not needed
+ */
+
