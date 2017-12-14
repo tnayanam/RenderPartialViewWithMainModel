16b1f571806d86fb939a6fc1115f97d963be3ac5
unknown <tnayanam@syr.edu>
2017-12-14 15:25:32 -0500

    New WCF SOlution Complete Flow - 1 Added, No Inheritance

---
+++ WCF/EmployeeService/EmployeeService/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeService
+{
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public string Gender { get; set; }
+
+        [DataMember]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
