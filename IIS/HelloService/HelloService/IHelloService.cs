9dc2a4d8bab36a61e6820104f7049eaaeeeeb160
tnayanam <nayatanuj@gmail.com>
2017-12-17 18:52:51 -0500

    Request-Reply Message Transfer

--- IIS/HelloService/HelloService/IHelloService.cs
+++ IIS/HelloService/HelloService/IHelloService.cs
@@ -6,7 +6,23 @@ namespace HelloService
     [ServiceContract]
     public interface IHelloService
     {
-        [OperationContract]
+        [OperationContract(IsOneWay = false)]
         string GetMessage(string name);
+        // above and below has same behavbiour
+        //[OperationContract]
+        //string GetMessage(string name);
     }
 }
+
+// there are three wayys by which client and service interact
+/*
+ * 1. Request Reply (default)
+ * 2. One Way
+ * 3. Duplex
+ * 
+ * If we do not put anything then its always request reply that means the clinet will wait for the service to processs once the service
+ * responds then only client processing will proceed further
+ * even if return tyoe is void then also the client will proceed when sercie has finished its processing
+ * 
+ * 
+*/
