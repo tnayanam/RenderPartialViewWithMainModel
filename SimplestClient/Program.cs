4a2eff691decff4dd4efca3a1321016766b99b43
unknown <tnayanam@syr.edu>
2017-12-19 16:53:08 -0500

    WCF Throttling

---
+++ SimplestClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimplestClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SimplestService.SimplestserviceClient client = new SimplestService.SimplestserviceClient();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                Thread thread = new Thread(client.DoWork);
+                thread.Start();
+            }
+        }
+    }
+}
---
+++ SimplestService/Simplestservice.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+
+namespace SimplestService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Simplestservice" in both code and config file together.
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall, ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class Simplestservice : ISimplestservice
+    {
+        public void DoWork()
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("Thread {0} processing request at {1}", Thread.CurrentThread.ManagedThreadId, DateTime.Now);
+        }
+    }
+}
