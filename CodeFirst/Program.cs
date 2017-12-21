61059ebcd735d9f3d7bef7eaec1f56d018dc361e
tnayanam <nayatanuj@gmail.com>
2017-12-21 03:07:05 -0500

    IIS WCF Hosting Adding Service Reference

--- CodeFirst/Program.cs
+++ CodeFirst/Program.cs
@@ -18,3 +18,8 @@ namespace CodeFirst
         }
     }
 }
+/*
+ * When you are trying to host on IIS a WCF Service then we need to add Service Reference to the client. And for that we need the URL of the service. GO to the
+ * IIS and find the serviec browse to the .svc file and in the browser the url that appears along with the WSDL docuemnt
+ * that url needs to go there.
+ */
