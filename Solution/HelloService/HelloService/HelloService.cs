6202d5894a61b86364b50a290975f6183e094d99
tnayanam <nayatanuj@gmail.com>
2017-12-21 01:34:25 -0500

    IIS Hosting COmpleted, Fixed IIS Hosting Issues

---
+++ Solution/HelloService/HelloService/HelloService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace HelloService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "HelloService" in both code and config file together.
+    public class HelloService : IHelloService
+    {
+        public string GetMessage(string message)
+        {
+            return "Hello " + message;
+        }
+    }
+}
+
+
+/*
+ * ISSUE IF YOU RUN INTO IIS HOSTING
+ * https://stackoverflow.com/questions/47914186/wcf-hosting-on-iis-throwing-exception-dont-have-permission-and-http-error-404/47914543?noredirect=1#comment82799042_47914543
+ * 
+ * 
+ */
