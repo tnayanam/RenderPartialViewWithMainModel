2e2f51f9c3f71059bcc9a39b671ee462a77aad52
tnayanam <nayatanuj@gmail.com>
2018-01-30 09:52:26 -0500

    Checked keyword in C#

---
+++ Test/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            int y = 2147483647;
+            int u = 10;
+
+            // checked is used to detect and capture - "The checked keyword is used to explicitly 
+            // enable overflow checking for integral-type arithmetic operations and conversions."
+            try
+            {
+                int h = checked(y + u);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
