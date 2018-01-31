026cf0e16c03319a891ee2945163c7c7cfc5fdd2
tnayanam <nayatanuj@gmail.com>
2018-01-30 23:09:14 -0500

    date time format

--- Test/Program.cs
+++ Test/Program.cs
@@ -8,19 +8,25 @@ namespace Test
         {
             Console.WriteLine("Hello World");
             int y = 2147483647;
-            int u = 10;
+            double u = 10.3456;
+
+            Console.WriteLine(string.Format("Temperature at {0:hh:mm tt} {0:MM/dd/yyyy} on {1:N2}", DateTime.Now, u));
+
+            //Temperature at 11:05 PM 01 - 30 - 2018 on 10.35
+
+
 
             // checked is used to detect and capture - "The checked keyword is used to explicitly 
             // enable overflow checking for integral-type arithmetic operations and conversions."
-            try
-            {
-                int h = checked(y + u);
-            }
-            catch (Exception e)
-            {
+            //try
+            //{
+            //    int h = checked(y + u);
+            //}
+            //catch (Exception e)
+            //{
 
-                Console.WriteLine(e.Message);
-            }
+            //    Console.WriteLine(e.Message);
+            //}
             Console.ReadLine();
         }
     }
